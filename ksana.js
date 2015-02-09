b0b918b09e05cfccb9ae912832c0a633039190ee
Yolinc <apple@AppledeMacBook-Pro.local>
2015-02-09 16:44:43 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,42 @@
+jsonp_handler({
+ "version": "1",
+ "build": 3,
+ "title": "myclock",
+ "minruntime": 1,
+ "baseurl": "http://yolinc@rawgit.com/yolinc/myclock/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  220,
+  62,
+  150799,
+  268382,
+  625422,
+  769,
+  35601,
+  113498,
+  97536
+ ],
+ "filedates": [
+  "2015-02-09T08:40:44.000Z",
+  "2015-02-09T08:23:56.000Z",
+  "2015-02-09T08:42:26.000Z",
+  "2015-02-09T08:23:56.000Z",
+  "2015-02-09T08:23:56.000Z",
+  "2015-02-09T08:38:22.000Z",
+  "2015-02-09T08:23:56.000Z",
+  "2015-02-09T08:23:56.000Z",
+  "2015-02-09T08:37:59.000Z"
+ ],
+ "date": "2015-02-09T08:42:27.325Z"
+})
